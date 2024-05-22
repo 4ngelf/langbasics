21422592cf228a21dd670b5a6a7a26f05597386a
Angel Fuenmayor <32184965+4ngelf@users.noreply.github.com>
2024-05-22 18:58:58 -0400

    chore(python): add tools for dev and docs
    Added tools include:
    - ruff (linter/formatter)
    - mkdocs-material
    - pytest

--- python/manage.py
+++ python/manage.py
@@ -7,21 +7,27 @@ from subprocess import run as run_default
 
 run = functools.partial(run_default, stdout=sys.stdout, stderr=sys.stderr)
 
+
 def parse_args() -> argparse.Namespace:
     prog = Path(sys.argv[0]).name
     parser = argparse.ArgumentParser(
-            prog=prog,
-            description=f"{prog} - project manager")
-    subparsers = parser.add_subparsers(required = True, dest="operation")
-    
+        prog=prog, description=f"{prog} - project manager"
+    )
+    subparsers = parser.add_subparsers(required=True, dest="operation")
+
     subparsers.add_parser("run", help="run project")
     subparsers.add_parser("test", help="test project")
-    install_parser = subparsers.add_parser("install", help="install this project")
+    install_parser = subparsers.add_parser(
+        "install", help="install this project"
+    )
 
     # TODO: add necessary arguments
-    install_parser.add_argument("--root", help="directory to install project into")
-    
+    install_parser.add_argument(
+        "--root", help="directory to install project into"
+    )
+
     return parser.parse_args()
+
 
 # TODO: match against parsed args and then invoke processes with run()
 def main() -> int:
@@ -31,6 +37,6 @@ def main() -> int:
 
     return 0
 
-    
+
 if __name__ == "__main__":
     sys.exit(main())
